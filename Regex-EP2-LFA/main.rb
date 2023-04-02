863003ed1424e13a171fe4c5711a508828e52426
Pedro Henrique <pholiveirabrito@hotmail.com>
2023-04-02 15:14:34 -0300

    fix: Melhoria na regex de data

--- Regex-EP2-LFA/main.rb
+++ Regex-EP2-LFA/main.rb
@@ -1,6 +1,4 @@
-# Dia: 22/03/2022
-
-# Horário: 10:00
+require 'date'
 
 def getting_value_of_regular_expression(text, regular_expression)
   text.match(regular_expression)
@@ -12,11 +10,11 @@ end
 puts("Digite o agendamento.\nExemplo: Agendar com José reunião às 10:00 amanhã #trabalho")
 $text_example = gets
 
-$action_regex = Regexp.new '([A-ZZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ]|[a-záàâãéèêíïóôõöúçñ])[a-zzáàâãéèêíïóôõöúçñ]+'
+$action_regex = Regexp.new '(ir)|([a-zA-Z]+)(?:ar|er|ir)'
 $pessoa_regex = Regexp.new '[A-ZZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ][a-zzáàâãéèêíïóôõöúçñ]+'
 $horario_regex = Regexp.new '(([0-9]?[0-9](((\:| )[0-9]{2})| [a-z]+))|[a-záàâãéèêíïóôõöúçñ]{2} [0-9]?[0-9](horas|hora)?)'
-$date_regex = Regexp.new '([0-9]{2}(( de [A-Za-z][a-z]+( de [0-9]{4})?)|(\/[0-9]{2}(\/[0-9]{4})?))|anteontem|antes de ontem|ontem|hoje|amanhã|depois de amanhã)'
-$tag_regex = Regexp.new '\#[a-z]+'
+$date_regex = Regexp.new '([0-9]{2}(( de [A-Za-z][a-z]+( de [0-9]{4})?)|(\/[0-9]{2}(\/[0-9]{4})?))|antes de ontem|anteontem|ontem|hoje|amanhã|depois de amanhã)'
+$tag_regex = Regexp.new '\#[aA-zZ]+'
 
 _action = getting_value_of_regular_expression($text_example, $action_regex).to_s
 $text_example = update_new_text($text_example, _action)
@@ -34,9 +32,26 @@ _tag = getting_value_of_regular_expression($text_example, $tag_regex).to_s
 $text_example = update_new_text($text_example, _tag)
 
 
+if 'AMANHÃ' == _date.upcase
+  hoje = Date.today
+  data = hoje.next_day.strftime('%d/%m/%Y')
+elsif 'DEPOIS DE AMANHÃ' == _date.upcase
+  hoje = Date.today
+  data = hoje.next_day.next_day.strftime('%d/%m/%Y')
+elsif 'HOJE' == _date.upcase
+  data = Date.today.strftime('%d/%m/%Y')
+elsif 'ONTEM' == _date.upcase
+  hoje = Date.today
+  data = hoje.prev_day.strftime('%d/%m/%Y')
+elsif 'ANTEONTEM' == _date.upcase or 'ANTES DE ONTEM' == _date.upcase
+  hoje = Date.today
+  data = hoje.prev_day.prev_day.strftime('%d/%m/%Y')
+else
+  data = _date
+end
 
-
-puts("Dia: " + _date)
+puts("Data: " + data)
 puts("Horário: " + _horario)
 puts("Pessoa: " + _pessoa)
 puts("Ação: " + _action)
+puts("Tag: " + _tag)
